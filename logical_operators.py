b30f6c0c79ab8c4dc9f1ad5d1a63bb5863256c4f
Nikhil <nikhil.panigrahi06@gmail.com>
2026-09-04 19:07:34 +0530

    Initial commit

---
+++ logical_operators.py
@@ -0,0 +1,21 @@
+#logical operators such as and , or run based on simple logics
+#or --> prints the true value or returns the first operant that is not false
+print(0 or 1)
+print (True or False)
+print ([] or False) #[]-->False
+print (False or [])
+print(False or False)
+print('hi' or 'hey')
+
+print("\n")
+#and only evaluates the second arguement if the first one is true
+#if the first one is false returns that statement
+print("\n")
+
+print(0 and 1)
+print (True and False)
+print ([] and False) #[]-->False
+print (False and [])
+print(False and False)
+print('hi' and 'hey')
+
