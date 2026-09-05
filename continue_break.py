f04d3b1df8d9747e7d194ef2b66843dda227da17
Nikhil <nikhil.panigrahi06@gmail.com>
2026-09-05 18:18:29 +0530

    usecase of continue and break in python

---
+++ continue_break.py
@@ -0,0 +1,9 @@
+for item in range(4):
+    if item ==2:
+        continue #continue means if condition is fulfilled skip the rest of the iteration and go to the next iteration of the loop
+    print(item)
+
+for value in range(5):
+    if value == 3:
+        break #break means if condition is fulfilled and Stop the entire loop immediately.
+    print(value)
