6368d3cd99d90c28cd71b96f2a80a5b3f57f6fb0
Nikhil <nikhil.panigrahi06@gmail.com>
2026-09-06 00:20:34 +0530

    modules usedin python

---
+++ modules.py
@@ -0,0 +1,15 @@
+# math for math utilities
+# re for regular expression
+# json to work with Json
+# datetime to work with dates
+# sqlite3 to utilise SQLite
+# os for Operating system utilities
+# random for random number generation
+# statistics for statistics utilities
+# requests to perform HTTP network request
+# http to create HTTP servers
+# urlib to manage URLs
+
+import math
+
+print(math.sqrt(4))
