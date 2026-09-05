dd3260e84c1fd44a76ec3ed1692e3ccf8d73122f
Nikhil <nikhil.panigrahi06@gmail.com>
2026-09-05 11:52:33 +0530

    Sets added

---
+++ sets.py
@@ -0,0 +1,8 @@
+set1 = {"Nikhil" , "Zelda"} #set as all unique values ,even if someting duplicates it shows only one in output terminal
+set2 = {"Zelda" , "Frieren"}
+intersection = set1 & set2
+print(intersection)
+union = set1 | set2
+print(union)
+#list(set(my_set)) --> is used to covert a list to a set
+  
