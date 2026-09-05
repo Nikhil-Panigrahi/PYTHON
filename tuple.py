2191a9cc3368b86c125a5332be100e4ef1ed183a
Nikhil <nikhil.panigrahi06@gmail.com>
2026-09-05 10:57:35 +0530

    tuples

---
+++ tuple.py
@@ -0,0 +1,11 @@
+names = ("Peter" , "Nikhil" , "tommy")
+print("steve" in names)
+print(names.index("Peter"))
+
+# in lists sorted function just sort the existing list
+# in tuple sorted function creates a new tuple
+
+print(sorted(names)) 
+print(names) # this shows the tuple that is sorted is a new one
+
+# in short tuples are immutable
