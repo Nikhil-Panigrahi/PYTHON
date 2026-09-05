54f7ec59aafab576406dc9610165531f46f7b121
Nikhil <nikhil.panigrahi06@gmail.com>
2026-09-05 08:37:06 +0530

    complex numbers and some basic maths functions

---
+++ complex_number.py
@@ -0,0 +1,12 @@
+num1 = 2+3j
+num2 = complex(2,3)
+print(num1)
+print(num2)
+print(num2.real , num2.imag)
+
+#abs() function acts as a mod |x|
+#round() function rounds the floating number to nearest integer or
+#round() can also be used as to round the number to any desired digit place
+
+a = 5.49
+print(round(a,1 )) #rounds to nearest decimal point
